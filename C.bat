5c93e7eea825ed1cc83525d9bcb8454be5eb69f7
shubham.garde397 <shubham.garde397@gmail.com>
2018-10-07 15:57:02 +0530

    test

---
+++ C.bat
@@ -0,0 +1,3 @@
+start "" "C:\Program Files\MongoDB\Server\4.0\bin\mongod" --dbpath "D:\Project\Data"
+ping localhost -n 10>nul
+start "" "C:\Program Files\MongoDB\Server\4.0\bin\mongo"
---
+++ Project.bat
@@ -0,0 +1,4 @@
+start A.bat
+start B.bat
+start C.bat
+start D.bat
